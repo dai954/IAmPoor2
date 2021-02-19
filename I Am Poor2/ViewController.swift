dadb3a3731d13d04998a39b9ba36d7197e0e97e4
dai954 <da0606ha@gmail.com>
2021-02-19 18:58:33 +0900

    first commit

---
+++ I Am Poor2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor2
+//
+//  Created by 石川大輔 on 2021/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
